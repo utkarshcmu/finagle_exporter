3e59d86dc8440a4d2fd66a3b56e36b3bd4a92267
utkarshcmu <utkarsh.cmu@gmail.com>
2017-12-19 14:41:05 -0800

    Restored defaults

--- README.md
+++ README.md
@@ -7,13 +7,16 @@ An exporter for [Twitter Finagle](https://twitter.github.io/finagle/), for Prome
 
 `mvn package` to build.
 
-`java -jar target/finagle_exporter-0.1-SNAPSHOT-jar-with-dependencies.jar localhost 9990` to run.
+`java -jar target/finagle_exporter-0.1-SNAPSHOT-jar-with-dependencies.jar` to run.
 
 All metrics are exported as gauges.
 
 ## Usage
 ```
-java -jar target/finagle_exporter-0.1-SNAPSHOT-jar-with-dependencies.jar <finagle_admin_host> <finagle_admin_port> [exporter_port]
+java -jar target/finagle_exporter-0.1-SNAPSHOT-jar-with-dependencies.jar [finagle_admin_host] [finagle_admin_port] [exporter_port]
 ```
 
-By default, exporter port is 9991
+### Defaults:
+finagle_admin_host: localhost
+finagle_admin_port: 9990
+exporter_port: 9991
--- src/main/java/io/github/utkarshcmu/WebServer.java
+++ src/main/java/io/github/utkarshcmu/WebServer.java
@@ -7,24 +7,28 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class WebServer {
 
-    public static String host;
-    public static int port;
-
     public static void main(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.err.println("Usage: jar <host> <port> [exporter_port]");
+        if (args.length < 0) {
+            System.err.println("Usage: jar [host] [port] [exporter_port]");
             System.exit(1);
         }
 
-        host = args[0];
-        port = Integer.parseInt(args[1]);
+        String host = "localhost";
+        int port = 9990;
+        if (args.length == 1) {
+        	host = args[0];
+        }
+        if (args.length == 2) {
+        	host = args[0];
+        	port = Integer.parseInt(args[1]);
+        }
         FinagleCollector collector = new FinagleCollector(host, port).register();
         
-        int exporterPort;
+        int exporterPort = 9991;
         if (args.length == 3) {
+        	host = args[0];
+        	port = Integer.parseInt(args[1]);
         	exporterPort = Integer.parseInt(args[2]);
-        } else {
-        	exporterPort = 9991;
         }
         
         Server server = new Server(exporterPort);
